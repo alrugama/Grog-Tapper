9c680b5c6577e156c2cc78a7f0d6ea6ad39ee1d4
Alex Rugama <alrugama@ucsc.edu>
2021-07-12 14:30:09 -0700

    test push

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ let config = {
 let game = new Phaser.Game(config);
 
 //set UI sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
+//let borderUISize = game.config.height / 15;
+//let borderPadding = borderUISize / 3;
 
 //Reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+//let keyF, keyR, keyLEFT, keyRIGHT;
